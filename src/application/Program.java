aef31ece05f7daa54ceb959f2f7d1a45de2899cc
Vagner Oliveira <vagner.natalicio@gmail.com>
2021-06-04 08:01:45 -0300

    Testando o clone do github

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Primeira atualização Github!");
 		System.out.println("Segunda Atualização!");
+		System.out.println("Testando o clone do github!");
 
 	}
 
